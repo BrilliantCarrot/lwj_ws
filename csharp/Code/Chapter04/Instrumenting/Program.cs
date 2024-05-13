fb8731d9d777dbb5d7be412d6478b568f542030a
BrilliantCarrot <leeyj950322@gmail.com>
2024-05-13 19:50:23 +0900

    2024/05/13 study

--- csharp/Code/Chapter04/Instrumenting/Program.cs
+++ csharp/Code/Chapter04/Instrumenting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
 using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 // 프로젝트 폴더에 텍스트 파일을 생성한다.
 Trace.Listeners.Add(new TextWriterTraceListener(
@@ -11,3 +12,23 @@ Trace.AutoFlush = true;
 
 Debug.WriteLine("Debug says, I am watching!");
 Trace.WriteLine("Trace says, I am watching!");
+
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(Directory.GetCurrentDirectory())
+  .AddJsonFile("appsettings.json",
+    optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+  displayName: "PacktSwitch",
+  description: "This switch is set via a JSON config.");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
