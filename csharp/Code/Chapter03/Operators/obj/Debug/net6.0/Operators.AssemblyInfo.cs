701090fb9c1310e6309f4a96113cc403ef55760c
BrilliantCarrot <leeyj950322@gmail.com>
2024-05-27 21:23:40 +0900

    .

--- csharp/Code/Chapter03/Operators/obj/Debug/net6.0/Operators.AssemblyInfo.cs
+++ csharp/Code/Chapter03/Operators/obj/Debug/net6.0/Operators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa59dde1d1164e1031bbcdc3be48bb7e920c31c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb199a3d575af4c94f1236e90b11fddb545e55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
