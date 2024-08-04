5a6b32c7fbd79c5d2b894cc7f66392ee9b950eaa
BrilliantCarrot <leeyj950322@gmail.com>
2024-08-04 23:18:15 +0900

    study in home on 08/04/2024

--- csharp/Code/Chapter06/PeopleApp/Program.cs
+++ csharp/Code/Chapter06/PeopleApp/Program.cs
@@ -15,13 +15,15 @@ baby1.Name = "Gary";
 Person baby2 = Person.Procreate(harry, jill);
 
 // call an operator
-// Person baby3 = harry * mary;
+Person baby3 = harry * mary;
 
-WriteLine($"{harry.Name} has {harry.Children.Count} children.");
-WriteLine($"{mary.Name} has {mary.Children.Count} children.");
-WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+// WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+// WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+// WriteLine($"{jill.Name} has {jill.Children.Count} children.");
 
-WriteLine(
-  format: "{0}'s first child is named \"{1}\".",
-  arg0: harry.Name,
-  arg1: harry.Children[0].Name);
+// WriteLine(
+//   format: "{0}'s first child is named \"{1}\".",
+//   arg0: harry.Name,
+//   arg1: harry.Children[0].Name);
+
+WriteLine($"5! is {Person.Factorial(5)}");
